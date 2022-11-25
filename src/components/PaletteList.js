f03f74275c156d45d232b1f33f7db09aedaf6c3e
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-11-26 00:14:48 +0800

    Set initial JSS styling for PaletteList

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,16 +1,51 @@
+import styled from "@emotion/styled";
+
 import MiniPalette from "./MiniPalette";
 
 // ________________________________________________________________
 
+const MainDiv = styled.div`
+  background-color: blue;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+const Nav = styled.nav`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  color: white;
+`;
+const PaletteDiv = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 30%);
+  grid-gap: 5%;
+`;
+
 const PaletteList = ({ palettes }) => {
   return (
-    <div>
-      <h1>Hue Box</h1>
-      <MiniPalette />
-      {palettes.map((palette) => (
-        <MiniPalette {...palette} />
-      ))}
-    </div>
+    <MainDiv>
+      <Container>
+        <Nav>
+          <h1>Hue Box</h1>
+        </Nav>
+        <PaletteDiv>
+          {palettes.map((palette) => (
+            <MiniPalette {...palette} />
+          ))}
+        </PaletteDiv>
+      </Container>
+    </MainDiv>
   );
 };
 
